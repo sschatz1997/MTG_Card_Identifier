bc7e2ee6b29a26391edb111e6ec5d17aafb074a5
sschatz1997 <s.schatz1997@gmail.com>
2021-01-04 20:16:02 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-APPS = "libpng-dev libjpeg-dev libtiff-dev zlib1g-dev python3-opencv tesseract-ocr libtesseract-dev"
 
 apt update 
-apt install -y $APPS
+apt install -y libpng-dev libjpeg-dev libtiff-dev zlib1g-dev python3-opencv tesseract-ocr libtesseract-dev
 pip3 install -r requirements.txt
