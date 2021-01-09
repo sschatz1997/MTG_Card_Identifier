6c41c1f30f051e1e78859b5c45a6788e5393b557
sschatz1997 <s.schatz1997@gmail.com>
2021-01-08 21:58:28 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,20 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sschatz1997/MTG_Card_Identifier",
-    packages=setuptools.find_packages(),
+    #packages=setuptools.find_packages(),
+    packages=[
+		'opencv_python',
+		'pandas',
+		'progress',
+		'pytesseract',
+		'Pillow',
+		'pytest',
+		'imagehash',
+		'art',
+		'colorama',
+		'requests',
+		'tabulate''
+	],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
