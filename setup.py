63bfa173daecf4e29500a83dc46cd5e8db75f218
sschatz1997 <s.schatz1997@gmail.com>
2021-01-08 21:20:43 -0500

    making a package

---
+++ setup.py
@@ -0,0 +1,23 @@
+# setup.py
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="MTG_Card_Identifier-sschatz1997",  # Replace with your own username
+    version="0.0.1",
+    author="Sam Schatz",
+    author_email="samsch1997@gmail.com",
+    description="Find magic cards based on the Name on the card.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sschatz1997/MTG_Card_Identifier",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7.3',
+)
--- tests/setup.py
+++
@@ -1,6 +0,0 @@
-# setup.py
-
-import os
-
-current_dir = os.path.dirname(os.path.realpath(__file__))
-print(current_dir)
